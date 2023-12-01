88a95aa86bddc8754365c855f54800a41b02eb5f
Kamiunk <327212678@qq.com>
2023-12-01 21:57:30 +0800

    添加

---
+++ cmake-build-debug-mingw/Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/githubFst_test/2023Exam_1/Google_tests/lib
+# Build directory: D:/githubFst_test/2023Exam_1/cmake-build-debug-mingw/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
